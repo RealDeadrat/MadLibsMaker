15664c08877e9cc728f621f29db5a26fa2841790
RealDeadrat <deadrat7078@gmail.com>
2019-09-08 18:52:54 -0500

    main activity version 1

---
+++ app/src/main/java/com/example/madlibsmaker/MadLibActivity.java
@@ -0,0 +1,15 @@
+package com.example.madlibsmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MadLibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mad_lib);
+    }
+
+}
